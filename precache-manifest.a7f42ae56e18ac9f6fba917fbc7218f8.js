bf6c582c892ceae6bb03df064a3c0eb4b0056140
nkkf <nelsonkenmochi@gmail.com>
2020-07-16 18:42:56 -0400

    Updates

---
+++ precache-manifest.a7f42ae56e18ac9f6fba917fbc7218f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41af709a5fdd2782d47e41ff2b258b80",
+    "url": "/mochila-digital-v2/index.html"
+  },
+  {
+    "revision": "d6f5c5e3ad35158b7374",
+    "url": "/mochila-digital-v2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d0e11c83b9ca3144182",
+    "url": "/mochila-digital-v2/static/js/2.4710a623.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/mochila-digital-v2/static/js/2.4710a623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f5c5e3ad35158b7374",
+    "url": "/mochila-digital-v2/static/js/main.42cd038d.chunk.js"
+  },
+  {
+    "revision": "fe1059ff49c15d2d8cdb",
+    "url": "/mochila-digital-v2/static/js/runtime-main.e19e513f.js"
+  }
+]);
